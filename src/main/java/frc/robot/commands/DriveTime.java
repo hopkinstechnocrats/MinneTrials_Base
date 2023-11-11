0d8dd9bfbfc262a8209d1af4f1ba500b16cccdf0
technocrats-software <44533697+Grig84@users.noreply.github.com>
2023-11-11 15:12:20 -0600

    Autonomous base code added

--- src/main/java/frc/robot/commands/DriveTime.java
+++ src/main/java/frc/robot/commands/DriveTime.java
@@ -7,25 +7,33 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
 
-public class DriveToWall extends CommandBase {
+public class DriveTime extends CommandBase {
   private DriveSubsystem subsystem;
+  private final double m_duration;
+  private final double m_speed;
+  private long m_startTime;
 
 /** Creates a new DriveToWall. */
-  public DriveToWall(DriveSubsystem subsystem) {
+  public DriveTime(DriveSubsystem subsystem, double speed, double duration) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
     this.subsystem = subsystem;
+   
+    m_speed = speed;
+    m_duration = duration;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_startTime = System.currentTimeMillis();
+  }
 
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    subsystem.drive(-0.4, -0.4);
+    subsystem.drive(m_speed, m_speed);
   }
 
   // Called once the command ends or is interrupted.
@@ -35,6 +43,6 @@ public class DriveToWall extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return subsystem.limitSwitch.get();
+   return (System.currentTimeMillis() - m_startTime) >= m_duration;
   }
 }
